760945c9e982bb85102336f9c0aff6d21a7e25bc
bsahinb <97312028+bsahinb@users.noreply.github.com>
2022-05-05 18:09:07 +0300

    Product AC1 is completed1

--- src/ProductTest_Burak.java
+++ src/ProductTest_Burak.java
@@ -1,2 +1,6 @@
 public class ProductTest_Burak {
+    public static void main(String[] args) {
+        System.out.println("Product class is created");
+        System.out.println("Product AC1 is passed");
+    }
 }
