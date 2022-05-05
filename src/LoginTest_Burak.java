22820c5824a996e44f22eb514f0d61551924dfee
bsahinb <97312028+bsahinb@users.noreply.github.com>
2022-05-05 17:46:50 +0300

    AC2 is completed

--- src/LoginTest_Burak.java
+++ src/LoginTest_Burak.java
@@ -2,6 +2,7 @@ public class LoginTest_Burak {
     public static void main(String[] args) {
         System.out.println("Login class is created");
         System.out.println("AC1 testing is PASSED");
+        System.out.println("AC2 testing is PASSED");
 
     }
 }
